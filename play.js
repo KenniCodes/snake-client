f9564bdc60a56a3edc5505c0b941e4900091eb0b
KenniCodes <kennedygosnell@outlook.com>
2023-09-27 20:23:14 -0700

    added function to ready if ^C was input, if so then exit connection

--- play.js
+++ play.js
@@ -1,6 +1,20 @@
 const { connect } = require('./client');
-
-
+const move = {
+  up: "Move: up"
+}
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+const handleUserInput = () => {
+  stdin.on("data", handleUserInput);
+  if (key === '\u0003') {
+    process.exit();
+  };
+};
 
 console.log("Connecting ...");
 connect();
